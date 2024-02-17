62617185ae095fa13a14512f47a8f1e6b447d141
adibmannan1 <adibmannan1@gmail.com>
2024-02-17 07:55:45 +0600

    header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className='poppins.variable}'>{children}</body>
+        <body className={poppins.variable}>{children}</body>
       </html>
     </ClerkProvider>
   );
--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
-
-const NavItems = () => {
+  import { headerLinks } from '@/constants'
+  import Link from 'next/link'
+  import React from 'react'
+  const NavItems = () => {
   return (
-    <div>NavItems</div>
-  )
-}
+    <ul className='md:flex-between flex w-full flex-col items-start gap-5 md:flex-row'>
+      {headerLinks.map(link => {
+        return (
+          <li
+            key={link.route}
+            className='flex-center p-medium-16 transition-colors duration-200 flex-center p-medium-16 whitespace-nowrap hover:text-[#5CD0DD]'
+          >
+            <Link href={link.route}>{link.label}</Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
-export default NavItems
+export default NavItems;
