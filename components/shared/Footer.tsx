02d615d36bdd5bd7b02ec9147994f66399b425ec
adibmannan1 <adibmannan1@gmail.com>
2024-02-17 08:10:57 +0600

    footer component

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,8 +1,17 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='border-t'>
+      <div className=' flex-center flex-between flex-col sm:flex-row gap-4 p-5 text-center'>
+        <Link href='/'>
+          <Image src="/assets/images/logo.svg" alt="FindNGo logo" width={30} height={30}/>
+        </Link>
+        <p>© 2024 FindNGo. All rights reserved.</p>
+      </div>
+    </footer>
   )
 }
 
